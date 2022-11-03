3cfd2a60618d042960918630e257d5f223f6d2ce
clooney780 <clooney780@gmail.com>
2022-11-03 11:14:00 +0000

    Refactored learn and begin learn

--- study/blueprints/learn/begin_learn.py
+++ study/blueprints/learn/begin_learn.py
@@ -4,21 +4,23 @@ from study.db import get_db
 
 from .main import bp
 
+def get_smallest_term_id(deck_id):
+    db = get_db()
+    terms = db.execute(
+        "SELECT id FROM term WHERE deck_id = ? ORDER BY id",
+        (str(deck_id),)
+    ).fetchall()
+    smallest_id = int(terms[0]['id'])
+    return smallest_id
+
 @bp.route("/<deck_id>/<routine_id>")
 @login_required
 @member_deck_view
 @member_routine_view
 def begin_learn(deck_id, routine_id):
-    db = get_db()
-    terms = db.execute(
-        "SELECT id FROM term WHERE deck_id = ?",
-        (str(deck_id),)
-    ).fetchall()
-    smallest_id = terms[0]['id']
-
+    smallest_term_id = get_smallest_term_id(deck_id)
     routine_position = 0
-
-    session['to_correct'] = []
+    session.pop('to_correct', None)
 
     return redirect(url_for("learn.learn", deck_id=deck_id, routine_id=routine_id,
-     term_id=smallest_id, routine_position=routine_position))
+     term_id=smallest_term_id, routine_position=routine_position))
--- study/blueprints/learn/learn.py
+++ study/blueprints/learn/learn.py
@@ -1,15 +1,47 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, g, flash
 from study.auth import login_required, member_routine_view, member_deck_view
 from study.db import get_db
 
 from .main import bp
 
+def get_steps(routine_id):
+    db = get_db()
+    routine = db.execute(
+        "SELECT steps FROM routine WHERE id = ?",
+        (str(routine_id),)
+    ).fetchone()
+    return routine["steps"]
+
+def get_terms(deck_id):
+    """Returns the terms linked to a deck ordered by id"""
+    db = get_db()
+    terms = db.execute(
+        "SELECT id FROM term WHERE deck_id = ? ORDER BY id",
+        (str(deck_id),)
+    ).fetchall()
+    return terms
+
+def get_highest_term_id(terms):
+    """Returns largest term id value assuming terms are ordered by id"""
+    last_term = terms[len(terms)-1]
+    last_id = int(last_term["id"])
+    return last_id
+
+def get_next_term_id(terms, term_id):
+    """Returns the id of the term after the term with the given id
+    assuming the terms are ordered by id"""
+    # stops just one before the last element to avoid index error
+    for i in range(len(terms)-1):
+        id = int(terms[i]["id"])
+        if id == term_id:
+            return terms[i + 1]
+    return -1
+
 def record_study_session(routine_id, deck_id):
     db = get_db()
-    user_id = g.user["id"]
     db.execute(
         "INSERT INTO study_session (user_id, routine_id, deck_id) VALUES (?, ?, ?)",
-        (str(user_id), str(routine_id), str(deck_id),)
+        (str(g.user["id"]), str(routine_id), str(deck_id),)
     )
     db.commit()
 
@@ -18,37 +50,45 @@ def record_study_session(routine_id, deck_id):
 @member_deck_view
 @member_routine_view
 def learn(deck_id, routine_id, term_id, routine_position):
-    routine_position = int(routine_position)
-
-    db = get_db()
-    steps = db.execute(
-        "SELECT steps FROM routine WHERE id = ?",
-        (str(routine_id),)
-    ).fetchone()['steps']
+    """Redirects user to next step in routine or ends study session
     
-    amount_of_steps = len(steps)
-    if routine_position >= amount_of_steps:
-        term_id = int(term_id)
-        terms = db.execute(
-            "SELECT id FROM term WHERE deck_id = ? ORDER BY id",
-            (str(deck_id),)
-        ).fetchall()
+    Routine position is the index of the step that it is currently on"""
+    routine_position = int(routine_position)
+    term_id = int(term_id)
+    deck_id = int(deck_id)
+    routine_id = int(routine_id)
 
-        if term_id == terms[len(terms)-1]["id"]:
+    # all the steps for the current routine
+    steps = get_steps()
+    amount_of_steps = len(steps)
+    
+    # if we are done with all the steps the position is equal to the amount of steps
+    if routine_position >= amount_of_steps:
+        
+        terms = get_terms(deck_id)
+        last_term_id = get_highest_term_id(terms)
+
+        # we are done with all the steps on the last term, so we are done with everything
+        # records the study session and redirects to index
+        if term_id == last_term_id:
             record_study_session(routine_id, deck_id)
             return redirect(url_for("/index"))
+        # otherwise we are done with the steps for this term but have more terms to do
+        # redirects to this function with the next term in line and back to start of routine
         else:
-            for i in range(len(terms)):
-                if terms[i]["id"] == term_id:
-                    next_term_id = terms[i + 1]["id"]
+            next_term_id = get_next_term_id(terms, term_id)
             return redirect(url_for("learn.learn", deck_id=deck_id, routine_id=routine_id,
          term_id=next_term_id, routine_position=0))
 
+    # redirects to the view for the current step
     current_step = steps[routine_position]
     step_views = {"a": "learn.ask", "c": "learn.correct", "f": "learn.flashcard",
     "m": "learn.choice", "b": "learn.fill_in_blanks"}
     if current_step in step_views:
         return redirect(url_for(step_views[current_step], deck_id=deck_id, routine_id=routine_id,
          term_id=term_id, routine_position=routine_position))
+    else:
+        error = "Error: Routine step does not exist"
+        flash(error)
 
     return redirect(url_for("/index"))
