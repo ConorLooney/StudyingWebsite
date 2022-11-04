f9817e139f1908b6f5999f5e15bb7c971b09bfde
clooney780 <clooney780@gmail.com>
2022-11-04 14:57:13 +0000

    Refactored and fixed fill in blanks

--- study/blueprints/learn/fill_in_blanks.py
+++ study/blueprints/learn/fill_in_blanks.py
@@ -1,21 +1,27 @@
-from flask import request, redirect, url_for, session, render_template, g
+from flask import request, render_template
 from study.auth import login_required, member_routine_view, member_deck_view
-from study.db import get_db, to_bit
 import random
 
 from .main import bp
+from .utility import get_term, add_to_queue_to_correct, record_attempt, redirect_to_next
 
-def queue_to_correct(term_id, given_answer):
-    session['to_correct'].append([term_id, given_answer])
-    session.modified = True
+def read_form(words):
+    answers = {}
+    for word_index in request.form.keys():
+        given_answer = request.form[word_index].replace("\r", "").replace("\n", "")
+        word_index = int(word_index)
+        correct_answer = words[word_index].replace("\r", "").replace("\n", "")
+        answers[correct_answer] = given_answer
+        
+    whole_answer = ""
+    for i in range(len(words)):
+        if str(i) in request.form.keys():
+            whole_answer += request.form[str(i)].replace("\r", "").replace("\n", "")
+        else:
+            whole_answer += words[i]
+        whole_answer += " "
 
-def get_question_answer(term_id):
-    db = get_db()
-    current_term = db.execute(
-        "SELECT * FROM term WHERE id = ?",
-        (str(term_id),)
-    ).fetchone()
-    return current_term["question"], current_term["answer"]
+    return answers, whole_answer
 
 def split_into_words(answer):
     # Word is defined as characters between spaces, characters including grammer
@@ -23,49 +29,55 @@ def split_into_words(answer):
     # "He? He, he was mad!" -> ["He?", "He," "he", "was", "mad!""]
     return answer.split(" ")
 
-def cover_words(words):
-    # if few words, my cover 0 by chance
-    # if not hitting this minimum then will add in some
-    covered_words_minimum = 1
-    # percentage of covered words of all words
-    # e.g. 0.25 means one quarter of words are covered
-    covered_words_proportion = 0.2
-    # default map of all uncovered
-    covered_words_map = [False] * len(words)
-    
-    covered_words_ideal_count = max(covered_words_minimum, covered_words_proportion * len(words))
-    
-    covered_count = covered_words_map.count(True)
-    while covered_count < covered_words_ideal_count:
-        covered_words_map[int(random.random() * len(covered_words_map))] = 1
-        covered_count = covered_words_map.count(True)
-    
-    return covered_words_map
+def blank_words(words, blank_minimum=1, blank_proportion=0.2):
+    blank_map = [False] * len(words)
+    ideal_blank_count = max(blank_minimum, int(blank_proportion * len(words)))
+    blank_count = 0
+    while blank_count < ideal_blank_count:
+        to_blank_index = int(random.random() * len(words))
+        # already blank
+        if blank_map[to_blank_index]:
+            continue
+        else:
+            words[to_blank_index] = "__" + str(blank_count + 1) + "__"
+            blank_map[to_blank_index] = True
+            blank_count += 1
+    return words, blank_map
 
 @bp.route("/<deck_id>/<routine_id>/<term_id>/<routine_position>/fill_in_blank", methods=("GET", "POST"))
 @login_required
 @member_deck_view
 @member_routine_view
 def fill_in_blanks(deck_id, routine_id, term_id, routine_position):
-    if request.method == "POST":
-        is_correct = True
-        for correct_answer in request.form.keys():
-            given_answer = request.form[correct_answer]
-            is_correct = is_correct and given_answer == correct_answer
-            if not is_correct:
-                queue_to_correct(term_id, given_answer)
-        db = get_db()
-        db.execute(
-            "INSERT INTO attempt (step, term_id, user_id, is_correct) VALUES (?, ?, ?, ?)",
-            ("f", str(term_id), str(g.user["id"]), str(to_bit(is_correct)),)
-        )
-        db.commit()
-        routine_position = int(routine_position) + 1
-        return redirect(url_for("learn.learn", deck_id=deck_id, routine_id=routine_id,
-            term_id=term_id, routine_position=routine_position))
-            
-    question, answer = get_question_answer(term_id)
+    # get the question
+    # get the answer
+    # fill in random words with __n__ where n is the amount of blank words to that one
+    # e.g. ich habe -> ich __1__ 
+    # e.g.  In Of Mice and Men John Steinbeck presents Slim as someone to be admired -> In Of Mice and Men John __1__ presents __2__ as someone to be __3__
+    # pass this to template to display each word
+    # then an input is made for each blank (using blank map to find the blanks)
+    # when the answers are given we get the index of the word each input is trying to be
+    # we know the answer and so get the correct answer from this index
+    term = get_term(term_id)
+    question = term["question"]
+    answer = term["answer"]
     words = split_into_words(answer)
-    covered_words_map = cover_words(words)
+
+    if request.method == "POST":
+        all_correct = True
+        
+        answers, whole_answer = read_form(words)
+        for correct_answer, given_answer in answers.items():
+            is_correct = given_answer == correct_answer
+            if not is_correct:
+                all_correct = False
+                add_to_queue_to_correct(term_id, whole_answer)
+        
+        record_attempt("b",term_id, all_correct)
+        return redirect_to_next(deck_id, routine_id, term_id, routine_position)
+
+    blanked_words, blanked_map = blank_words(words)
+
+
     return render_template("learn/fill_in_blanks.html", question=question,
-    words=words, covered_words_map=covered_words_map)
+    blanked_words=blanked_words, blanked_map=blanked_map)
