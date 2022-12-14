92a6db4d49e9634074de299951669af11e1e88e9
clooney780 <clooney780@gmail.com>
2022-12-14 18:41:11 +0000

    refactored create page for routine

--- study/blueprints/routines/create.py
+++ study/blueprints/routines/create.py
@@ -3,6 +3,7 @@ from study.db import get_db
 from study.auth import login_required
 
 from .main import bp
+from study.validation import presence_check, lookup_check
 
 class Step:
 
@@ -10,38 +11,51 @@ class Step:
         self.name = name
         self.abbreviation = abbreviation
 
+def read_form():
+    """Returns the form data formatted"""
+    name = request.form["routine_name"]
+    steps = request.form["steps"]
+    mode = request.form["mode"]
+    is_step_mode = 1 if mode == "step_mode" else 0
+    return name, steps, is_step_mode
+
+def validate(name, steps, is_step_mode):
+    """Returns false if any of the data is invalid
+    
+    Also displays an error if the data is invalid"""
+    if not presence_check(name):
+        flash("Error: Invalid routine name")
+        return False
+    
+    if not presence_check(steps):
+        flash("Error: Invalid routine steps")
+        return False
+    
+    if not lookup_check(is_step_mode, [0, 1]):
+        flash("Error: Invalid step or term mode")
+        return False
+    
+    return True
+
+def insert_routine_in_db(name, steps, is_step_mode):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(
+        "INSERT INTO routine (owner_id, title, steps, is_step_mode) VALUES (?, ?, ?, ?)",
+        (str(g.user["id"]), name, steps, str(is_step_mode),)
+    )
+    db.commit()
+    routine_id = cursor.lastrowid
+    return routine_id
+
 @bp.route("/create", methods=("GET", "POST"))
 @login_required
 def create():
     if request.method == "POST":
-        routine_name = request.form["routine_name"]
-        steps = request.form["steps"]
-        mode = request.form["mode"]
-        is_step_mode = 1 if mode == "step_mode" else 0
-
-        error = None
-        if routine_name is None:
-            error = "Routine name is required"
-        if steps is None:
-            error = "Routine steps are required"
-
-        if error is None:
-            db = get_db()
-            cursor = db.cursor()
-            try:
-
-                cursor.execute(
-                    "INSERT INTO routine (owner_id, title, steps, is_step_mode) VALUES (?, ?, ?, ?)",
-                    (str(g.user['id']), routine_name, steps, str(is_step_mode))
-                )
-                db.commit()
-                routine_id = cursor.lastrowid
-
-                return redirect(url_for("routines.view_routine", routine_id=routine_id))
-            except db.IntegrityError:
-                error = "Routine name must be unique"
-
-        flash(error)
+        name, steps, is_step_mode = read_form()
+        if validate(name, steps, is_step_mode):
+            routine_id = insert_routine_in_db(name, steps, is_step_mode)
+            return redirect(url_for("routines.see_one", routine_id=routine_id))
 
     avaliable_steps = [
         Step("Ask", "a"),
--- study/validation.py
+++ study/validation.py
@@ -4,4 +4,11 @@
 def presence_check(data):
     if data is None:
         return False
+    if type(data) == str and len(data) == 0:
+        return False
+    return True
+
+def lookup_check(data, data_set):
+    if data not in data_set:
+        return False
     return True
