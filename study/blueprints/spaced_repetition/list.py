db8d62c08699bda70961c8b9a327ecfa6007b527
clooney780 <clooney780@gmail.com>
2023-01-03 21:46:41 +0000

    Styled and tweaked spaced repetition pages

--- study/blueprints/spaced_repetition/list.py
+++ study/blueprints/spaced_repetition/list.py
@@ -15,8 +15,41 @@ def get_user_spaced_repetition_settings():
 
     return spaced_repetition_settings
 
+def get_decks_dict(spaced_repetition_settings):
+    """Return dictionary of deck ids to decks for each deck id of each spaced
+    repetition setting in the given list"""
+    db = get_db()
+    decks = {}
+    for spaced_repetition_setting in spaced_repetition_settings:
+        deck_id = int(spaced_repetition_setting["deck_id"])
+        deck = db.execute(
+            "SELECT * FROM deck WHERE id = ?",
+            (str(deck_id),)
+        ).fetchone()
+        decks[deck_id] = deck
+    return decks
+
+def get_routines_dict(spaced_repetition_settings):
+    """Return dictionary of routine ids to routines for each routine id of each spaced
+    repetition setting in the given list"""
+    db = get_db()
+    routines = {}
+    for spaced_repetition_setting in spaced_repetition_settings:
+        routine_id = int(spaced_repetition_setting["routine_id"])
+        routine = db.execute(
+            "SELECT * FROM routine WHERE id = ?",
+            (str(routine_id),)
+        ).fetchone()
+        routines[routine_id] = routine
+    return routines
+
 @bp.route("/list")
 def list():
     spaced_repetition_settings = get_user_spaced_repetition_settings()
+
+    decks = get_decks_dict(spaced_repetition_settings)
+    routines = get_routines_dict(spaced_repetition_settings)
+
     return render_template("spaced_repetition/list.html",
-    spaced_repetition_settings=spaced_repetition_settings)
+    spaced_repetition_settings=spaced_repetition_settings,
+    decks=decks, routines=routines)
--- study/blueprints/spaced_repetition/view.py
+++ study/blueprints/spaced_repetition/view.py
@@ -13,8 +13,28 @@ def get_spaced_repetition_setting(deck_id, routine_id):
     ).fetchone()
     return spaced_repetition_setting
 
+def get_deck(deck_id):
+    db = get_db()
+    deck = db.execute(
+        "SELECT * FROM deck WHERE id = ?",
+        (str(deck_id),)
+    ).fetchone()
+    return deck
+
+def get_routine(routine_id):
+    db = get_db()
+    routine = db.execute(
+        "SELECT * FROM routine WHERE id = ?",
+        (str(routine_id),)
+    ).fetchone()
+    return routine
+
 @bp.route("/view/<deck_id>/<routine_id>")
 def view(deck_id, routine_id):
     spaced_repetition_setting = get_spaced_repetition_setting(deck_id, routine_id)
+
+    deck = get_deck(deck_id)
+    routine = get_routine(routine_id)
+
     return render_template("spaced_repetition/view.html",
-    spaced_repetition_setting=spaced_repetition_setting)
+    spaced_repetition_setting=spaced_repetition_setting, deck=deck, routine=routine)
